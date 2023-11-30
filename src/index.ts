f4f331c00c5f597f7203a366d65af564d60d42a6
TomasEng <tomas.engebretsen@gmail.com>
2023-11-30 21:26:10 +0100

    Export things

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { generalMatcher } from './sort-matchers/generalMatcher';
+export { MatchSort } from './classes/MatchSort';
+export { StringMatchPredicate } from './static-classes/StringMatchPredicate';
+export { StringMatchRank } from './static-classes/StringMatchRank';
+export { StringTransform } from './static-classes/StringTransform';
